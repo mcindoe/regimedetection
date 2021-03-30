d31a21a2e81cecd88476874bb4dfbda63457325e
mcindoe <conormcindoe1@gmail.com>
2021-03-30 17:11:47 +0100

    Add type-hinting to similarities functions

--- src/similarities.py
+++ src/similarities.py
@@ -9,8 +9,19 @@ decreasing functions
 import math
 import numpy as np
 
+from typing import Callable
 
-def make_gaussian_similarity(sigma):
+
+def make_gaussian_similarity(sigma: np.float) -> Callable[[np.float], np.float]:
+    """
+    Produces the gaussian similarity function corresponding to the
+    input sigma value.
+
+    Used to create a function of one argument, as expected by the
+    multiscale-k-prototypes algorithm in the clustering module,
+    from a specified sigma
+    """
+
     sigma_squared = sigma * sigma
 
     def gaussian_similarity(x):
@@ -19,6 +30,15 @@ def make_gaussian_similarity(sigma):
     return gaussian_similarity
 
 
-def make_gaussian_similarity_from_percentile(distances, percentile):
+def make_gaussian_similarity_from_percentile(
+    distances: np.ndarray, percentile: np.float
+) -> Callable[[np.float], np.float]:
+    """
+    Produce the gaussian similarity function when sigma is chosen
+    to be the specified percentile of the distances
+
+    Percentile must be a value in [0, 100]
+    """
+
     sigma = np.percentile(distances, percentile)
     return make_gaussian_similarity(sigma)
